c8de4c8e44febbf3d5999cd2fde2fd5490f44992
NigelDeluxe <i.suvorov.2110@gmail.com>
2025-06-23 11:33:51 +0200

    move implicitly wait from conftest.py to base_page.py add new method to main_page.py add new test to test_main_page.py

--- conftest.py
+++ conftest.py
@@ -14,7 +14,6 @@ def browser(request):
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     print("\nstart browser for test..")
     browser = webdriver.Chrome(options=options)
-    browser.implicitly_wait(10)
     yield browser
     print("\nquit browser..")
     browser.quit()
